20ef71de2ef977f27e6feeb0874d6ddb5a6b88fd
Jonay Luis Alonso <36797607+chonay9@users.noreply.github.com>
2019-05-22 13:12:51 +0200

    Kata Success
    Read de txt file

---
+++ code.js
@@ -0,0 +1,20 @@
+//Solution
+function digPow(n, p){
+  var digits = (""+n).split("");
+  var resultado=0;
+  for (var i=0;i<digits.length;i++){
+    resultado=resultado+(Math.pow(parseInt(digits[i]),(p+i)));
+  }
+  console.log(resultado);
+  var compare = resultado / parseInt(n);
+  if(resultado==(parseInt(n)*compare)){
+    return compare;
+  }
+  else
+    return -1;
+}
+
+//Sample Tests
+Test.assertEquals(digPow(89, 1), 1)
+Test.assertEquals(digPow(92, 1), -1)
+Test.assertEquals(digPow(46288, 3), 51)
